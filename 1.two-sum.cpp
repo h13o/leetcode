a9419fc00272db1ed7b06373c2b37f5544082dfe
Hasegawa Rintaro <aplboicundaze@gmail.com>
2019-12-12 21:55:05 +0900

    add solution

--- 1.two-sum.cpp
+++ 1.two-sum.cpp
@@ -10,6 +10,14 @@ class Solution
 public:
     vector<int> twoSum(vector<int> &nums, int target)
     {
+        unordered_map<int, int> umap;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (umap.find(nums[i]) != umap.end())
+                return {i, umap[nums[i]]};
+            umap[target - nums[i]] = i;
+        }
+        return {INT_MIN, INT_MIN};
     }
 };
 // @lc code=end
